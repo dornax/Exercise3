f0ff02d68b3b8d3893e13a61949f95cd33098b9c
Björn Hansen <dornax@msn.com>
2023-09-21 10:44:54 +0200

    3.1.1

---
+++ Exercise3/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Person
+    {
+
+        public Person(string fName, string lName)
+        {
+
+            FName = fName;
+            LName = lName;
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0) _age = value;
+                else
+                    throw new ArgumentException("Age should be greater than 0");
+            }
+        }
+
+        private string _fName;
+
+        public string FName
+        {
+            get { return _fName; }
+            set
+            {
+                int fNameLength = value.Length;
+                if (fNameLength >= 2 && fNameLength <= 10) _fName = value;
+                else
+                    throw new ArgumentException(
+                        "Firstname should be 2 to 10 characters in length.");
+            }
+        }
+
+        public string _lName;
+
+        public string LName
+        {
+            get { return _lName; }
+            set
+            {
+
+                int lNameLength = value.Length;
+                if (lNameLength >= 3 && lNameLength <= 15) _lName = value;
+                else
+                    throw new ArgumentException(
+                        "Lastname should be 3 to 15 characters in length.");
+            }
+        }
+
+        private double _height;
+
+        public double Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        private double _weight;
+
+        public double Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+    }
+}
--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Exercise3
+﻿using System;
+
+namespace Exercise3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+
+            try
+            {
+                Person person = new Person("John", "Smith");
+                person.Age = 25;
+                Console.WriteLine($"{person.FName} {person.LName} {person.Age}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
